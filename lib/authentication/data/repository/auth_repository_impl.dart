d0283e1cb6816015752c2643f86160129b20cf89
apple <mangelachinmay7@gmail.com>
2025-05-22 23:36:20 +0530

    Feat: Implement AuthRepository and its concrete implementation
    This commit introduces the `AuthRepository` abstract class and its concrete implementation `AuthRepositoryImpl`.
    
    The following changes were made:
    - Define the `AuthRepository` abstract class with methods for:
        - `signInWithEmail`
        - `signUpWithEmail`
        - `signOut`
        - `sendEmailVerification`
        - `resetPassword`
    - Create `AuthRepositoryImpl` which implements `AuthRepository`.
    - `AuthRepositoryImpl` depends on `AuthRemoteDataSource` to perform authentication operations.
    - Rename `signIn` to `signInWithEmail` and `signUp` to `signUpWithEmail` in `AuthRepository` for clarity.

---
+++ lib/authentication/data/repository/auth_repository_impl.dart
@@ -0,0 +1,36 @@
+import 'package:bloc_practice/authentication/data/data_source/remote/auth_remote_data_source.dart';
+import 'package:bloc_practice/authentication/data/model/end_user.dart';
+import 'package:bloc_practice/authentication/domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  AuthRepositoryImpl({required AuthRemoteDataSource authRemoteDataSource})
+      : _authRemoteDataSource = authRemoteDataSource;
+
+  final AuthRemoteDataSource _authRemoteDataSource;
+
+  @override
+  Future<void> resetPassword(String email) async {
+    await _authRemoteDataSource.forgotPassword(email: email);
+  }
+
+  @override
+  Future<void> sendEmailVerification() async {
+    await _authRemoteDataSource.sendEmailVerification();
+  }
+
+  @override
+  Future<void> signInWithEmail(String email, String password) async {
+    await _authRemoteDataSource.signInWithEmail(
+        email: email, password: password);
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _authRemoteDataSource.signOut();
+  }
+
+  @override
+  Future<void> signUpWithEmail(EndUser user) async {
+    await _authRemoteDataSource.signUpWithEmail(user: user);
+  }
+}
--- lib/authentication/domain/repository/auth_repository.dart
+++ lib/authentication/domain/repository/auth_repository.dart
@@ -3,8 +3,8 @@
 import 'package:bloc_practice/authentication/data/model/end_user.dart';
 
 abstract class AuthRepository {
-  Future<void> signIn(String email, String password);
-  Future<void> signUp(EndUser user);
+  Future<void> signInWithEmail(String email, String password);
+  Future<void> signUpWithEmail(EndUser user);
   Future<void> signOut();
   Future<void> sendEmailVerification();
   Future<void> resetPassword(String email);
